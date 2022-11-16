e7d1e082a7ef92d2548d672b1643d25dccc82c46
Isabela Ortiz <i.ortiz@uniandes.edu.co>
2022-11-16 10:07:06 -0500

    cambios detail

--- src/components/detail.js
+++ src/components/detail.js
@@ -3,7 +3,7 @@ const { useEffect, useState } = require("react");
 
 export default function Detail() {
     const params = useParams();
-    const [mascota, setMascotas] = useState([]);
+    const [mascota, setMascotas] = useState({});
     useEffect(() => {
         const URL =
             "https://gist.githubusercontent.com/josejbocanegra/829a853c6c68880477697acd0490cecc/raw/99c31372b4d419a855e53f0e891246f313a71b20/mascotas.json";
@@ -17,6 +17,11 @@ export default function Detail() {
     return (
         <div>
              <h1>{mascota.nombre}</h1>
+             <img src={mascota.foto} style={{
+                maxHeight: '50%',
+                maxWidth: '50%'
+            }}></img>
+             <h2>{mascota.raza}</h2>
         </div>
     );
 }
